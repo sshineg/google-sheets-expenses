8fb580e301a3f81cafd966d71ed1ab4b00a02227
Alexander <sshineg@gmail.com>
2019-03-15 20:23:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 function buttonValid() {
   var isSumm = document.getElementsByName("summ")[0].value;
+  var description = document.getElementsByName("description")[0].value;
   if (!isSumm) {
   	alert("Введите сумму");
+  } else if (!description) {
+    alert("Enter desc");
   } else {
-    alert(isSumm);
+    alert(isSumm + "\n" + description);
   }
 }
+
