3501a546098b7dae4c9dc1ad8585e7724ece6315
Alexander <sshineg@gmail.com>
2019-03-18 16:09:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 function onExpensesSubmit() {
   var summ = document.getElementsByName("summ")[0].value;
   var description = document.getElementsByName("description")[0].value;
+  var currency = document.getElementsByName("currency")[].value;
   if (!summ) {
   	alert("Введите сумму");
   } else if (!description) {
     alert("Введите описание");
   } else {
-    alert("Описание: " + description + "\n" + "Сумма: " + summ);
+    alert("Описание: " + description + "\n" + "Сумма: " + summ + currency);
   }
 }
 
